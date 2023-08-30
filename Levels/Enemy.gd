15d30655761c4fc7aa995d801ef6470db65c9637
19sglover <103233621+19sglover@users.noreply.github.com>
2023-08-31 09:26:09 +1200

    Changed bullet system.
    Changed the bullets so that it instead creates instances of bullets and then deletes them when they leave the screen or hit a body.

--- Levels/Enemy.gd
+++ Levels/Enemy.gd
@@ -4,43 +4,33 @@ extends Area2D
 @export var move_direction : Vector2 
 @onready var Player = get_node("/root/Test_Scene/Player")
 @onready var enemy = get_node("/root/Test_Scene/Enemy/Bullet")
+@onready var security = get_node("/root/Test_Scene/Enemy/SecurityGuard2")
 
+var can_fire = true
+var bullet = preload("res://bullet.tscn")
 var start_position : Vector2
 var target_postition : Vector2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_position = global_position
 	target_postition = start_position + move_direction
+	start_position = security.global_position
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Player.in_enemy_space == true or Player.in_enemy_space_other == true: 
-		_bullet_movement(delta)
-	else: 
-		get_node("/root/Test_Scene/Enemy/bullet_shape").disabled == true
-		enemy.visible == false
-		
-func _bullet_movement(delta):
-	get_node("/root/Test_Scene/Enemy/bullet_shape").disabled == false
-	
-	if Player.in_enemy_space_other == true:
-		get_node("/root/Test_Scene/SecurityGuard2").scale.x = -1
-		target_postition = start_position - move_direction
-	elif Player.in_enemy_space_other == false:
-		get_node("/root/Test_Scene/SecurityGuard2").scale.x = 1
-		target_postition = start_position + move_direction
-		
-	global_position = global_position.move_toward(target_postition, move_velocity * delta)
-	if global_position == target_postition:
-		enemy.visible = false 
-		global_position = start_position
-		enemy.visible = true
+func _physics_process(delta):
+	if (Player.in_enemy_space == true or Player.in_enemy_space_other == true) and can_fire == true: 
+		var bullet_instance = bullet.instantiate()
+		bullet_instance.global_position = start_position
+		get_parent().add_child(bullet_instance)
+		can_fire = false
+		await get_tree().create_timer(2).timeout
+		can_fire = true
 		
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		print("Collision")
 		Player.souls -= 25
-		global_position == start_position
+		global_position = start_position
 		enemy.visible = false 
---
+++ bullet.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+@export var speed = 150
+@onready var Player = get_node("/root/Test_Scene/Player")
+@onready var guard = get_node("/root/Test_Scene/Enemy/SecurityGuard2")
+
+@onready var bul: AnimatedSprite2D = $AnimatedSprite2D
+
+var x = 0
+
+func _ready():
+	set_as_top_level(true)
+
+func _process(delta):
+	if x == 1:
+		position += (Vector2.RIGHT*speed*-1) * delta
+	elif x == -1:
+		position += (Vector2.RIGHT*speed) * delta
+	elif Player.in_enemy_space == true:
+		x = 1
+	elif Player.in_enemy_space_other == true:
+		x = -1
+	
+func _physics_process(delta):
+	if Player.in_enemy_space == true:
+		guard.scale.x = 1
+		bul.flip_h = true
+	elif Player.in_enemy_space_other == true:
+		guard.scale.x = -1
+	
+	await get_tree().create_timer(0.01).timeout
+	set_physics_process(false)
+	
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+func _on_body_entered(body):
+	queue_free()
+	if body.is_in_group("Player"):
+		Player.souls -= 25
+	
